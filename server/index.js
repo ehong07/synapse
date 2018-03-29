1bba377830f4e3025385f0cf7c255a9561732fff
Easak Hong <easakhong@gmail.com>
2018-03-28 22:16:51 -0700

    Final cleanup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('../db');
 const dotenv = require('dotenv');
 const SynapsePay = require('synapsepay');
 
